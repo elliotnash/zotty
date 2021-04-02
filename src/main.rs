693f257d0069c8605c9ba6c2ab8b30f969aeae5f
Elliot Nash <elliotnash@gmail.com>
2021-04-01 18:10:45 -0700

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use std::{env, sync::Arc};
+
+use serenity::{
+    async_trait,
+    client::bridge::gateway::ShardManager,
+    framework::{
+        StandardFramework,
+        standard::macros::group,
+    },
+    model::gateway::Ready,
+    prelude::*,
+};
+
+mod commands;
+use commands::{
+    help::*
+};
+
+pub struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<Mutex<ShardManager>>;
+}
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[group]
+#[commands(help)]
+struct General;
+
+//init client
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN")
+        .expect("Expected a token in the environment");
+
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~"))
+        .group(&GENERAL_GROUP);
+
+    let mut client = Client::builder(&token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Err creating client");
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ShardManagerContainer>(client.shard_manager.clone());
+    }
+
+    let shard_manager = client.shard_manager.clone();
+
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.expect("Could not register ctrl+c handler");
+        println!("ETechBot is shutting down");
+        shard_manager.lock().await.shutdown_all().await;
+    });
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
