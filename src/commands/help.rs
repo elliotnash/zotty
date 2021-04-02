aa011b170eca153a44b697eb097f21cfceac132d
Elliot Nash <elliotnash@gmail.com>
2021-04-01 20:33:53 -0700

    damnit

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -14,21 +14,24 @@ async fn help(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
             println!("args is test")
         }
         Some(_) => {
-            help_main(&ctx.http, msg).await?;
+            help_main(ctx, msg).await?;
         }
         None => {
-            help_main(&ctx.http, msg).await?;
+            help_main(ctx, msg).await?;
         }
     }
 
     Ok(())
 }
 
-async fn help_main(http: &Http, msg: &Message) -> CommandResult {
+async fn help_main(ctx: &Context, msg: &Message) -> CommandResult {
 
-    msg.channel_id.send_message(http, |m| {
+    let thumbnail_url = msg.guild(&ctx.cache).await.unwrap().icon_url();
+
+    msg.channel_id.send_message(&ctx.http, |m| {
         m.embed(|e| {
             e.color(0xD3A6F6);
+            if let Some(url) = thumbnail_url {e.thumbnail(url);};
             e.title("**Help**");
             e.field("**Moderator**", format!("`{}help moderator`", CONFIG.prefix), true)
         })
