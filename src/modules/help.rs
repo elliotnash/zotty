4f0f7d4c6b8e86a272bf86643b5d7ee03a90ab56
Elliot Nash <elliotnash@gmail.com>
2021-04-07 14:35:12 -0700

    pass prefix as param

--- src/modules/help.rs
+++ src/modules/help.rs
@@ -7,20 +7,21 @@ use crate::commands::{Args, CommandResult};
 use crate::CONFIG;
 
 pub async fn help(ctx: Context, msg: Message, args: Args) {
+    let prefix = CONFIG.get().unwrap().prefix.clone();
     match args.current() {
         Some("mod") | Some("moderator") => {
-            help_moderator(&ctx, &msg).await
+            help_moderator(&ctx, &msg, &prefix).await
         }
         Some("music") => {
-            help_music(&ctx, &msg).await
+            help_music(&ctx, &msg, &prefix).await
         }
         Some("utils") | Some("utilities") => {
-            help_utilities(&ctx, &msg).await
+            help_utilities(&ctx, &msg, &prefix).await
         }
         _ => {
-            help_main(&ctx, &msg).await
+            help_main(&ctx, &msg, &prefix).await
         }
-    }.expect("Error dispatching help command")
+    }.expect("Error dispatching help command");
 }
 
 pub async fn send_usage(ctx: &Context, msg: &Message, error: &str, usage: &str) {
@@ -42,7 +43,7 @@ pub async fn send_error(ctx: &Context, msg: &Message, error: &str) {
     }).await.expect("Failed to send message");
 }
 
-async fn help_main(ctx: &Context, msg: &Message) -> CommandResult {
+async fn help_main(ctx: &Context, msg: &Message, prefix: &str) -> CommandResult {
 
     let thumbnail_url = msg.guild(&ctx.cache).await.unwrap().icon_url();
 
@@ -51,16 +52,16 @@ async fn help_main(ctx: &Context, msg: &Message) -> CommandResult {
             e.color(CONFIG.get().unwrap().colours.help);
             if let Some(url) = thumbnail_url {e.thumbnail(url);};
             e.title("**Help**");
-            e.field("**Moderator**", format!("`{}help moderator`", CONFIG.get().unwrap().prefix), true);
-            e.field("**Music**", format!("`{}help music`", CONFIG.get().unwrap().prefix), true);
-            e.field("**Utilities**", format!("`{}help utilities`", CONFIG.get().unwrap().prefix), true)
+            e.field("**Moderator**", format!("`{}help moderator`", prefix), true);
+            e.field("**Music**", format!("`{}help music`", prefix), true);
+            e.field("**Utilities**", format!("`{}help utilities`", prefix), true)
         })
     }).await?;
 
     Ok(())
 }
 
-async fn help_moderator(ctx: &Context, msg: &Message) -> CommandResult {
+async fn help_moderator(ctx: &Context, msg: &Message, prefix: &str) -> CommandResult {
 
     let thumbnail_url = msg.guild(&ctx.cache).await.unwrap().icon_url();
 
@@ -69,21 +70,21 @@ async fn help_moderator(ctx: &Context, msg: &Message) -> CommandResult {
             e.color(CONFIG.get().unwrap().colours.moderator);
             if let Some(url) = thumbnail_url {e.thumbnail(url);};
             e.title("**Help Moderator**");
-            e.field(format!("`{}mute [user]`", CONFIG.get().unwrap().prefix), "Mutes the mentioned user", false);
-            e.field(format!("`{}kick [user]`", CONFIG.get().unwrap().prefix), "Kicks the mentioned user", false);
-            e.field(format!("`{}ban [user]`", CONFIG.get().unwrap().prefix), "Bans the mentioned user", false);
-            e.field(format!("`{}unmute [user]`", CONFIG.get().unwrap().prefix), "Unmutes the mentioned user", false);
-            e.field(format!("`{}purge [number of messages]`", CONFIG.get().unwrap().prefix), "Clears a specified amount of messages", false);
-            e.field(format!("`{}stream [message]`", CONFIG.get().unwrap().prefix), "Sets the bot's status to streaming the specified message", false);
-            e.field(format!("`{}setup [help | discord]`", CONFIG.get().unwrap().prefix), 
-                format!("Sets up the discord bot. See `{}setup help` for more information", CONFIG.get().unwrap().prefix), false)
+            e.field(format!("`{}mute [user]`", prefix), "Mutes the mentioned user", false);
+            e.field(format!("`{}kick [user]`", prefix), "Kicks the mentioned user", false);
+            e.field(format!("`{}ban [user]`", prefix), "Bans the mentioned user", false);
+            e.field(format!("`{}unmute [user]`", prefix), "Unmutes the mentioned user", false);
+            e.field(format!("`{}purge [number of messages]`", prefix), "Clears a specified amount of messages", false);
+            e.field(format!("`{}stream [message]`", prefix), "Sets the bot's status to streaming the specified message", false);
+            e.field(format!("`{}setup [help | discord]`", prefix), 
+                format!("Sets up the discord bot. See `{}setup help` for more information", prefix), false)
         })
     }).await?;
 
     Ok(())
 }
 
-async fn help_music(ctx: &Context, msg: &Message) -> CommandResult {
+async fn help_music(ctx: &Context, msg: &Message, prefix: &str) -> CommandResult {
 
     let thumbnail_url = msg.guild(&ctx.cache).await.unwrap().icon_url();
 
@@ -92,21 +93,21 @@ async fn help_music(ctx: &Context, msg: &Message) -> CommandResult {
             e.color(CONFIG.get().unwrap().colours.music);
             if let Some(url) = thumbnail_url {e.thumbnail(url);};
             e.title("**Help Music**");
-            e.field(format!("`{}connect [user]`", CONFIG.get().unwrap().prefix), format!("Joins the voice channel the user is in \n\
-            *aliases:* `{}join`", CONFIG.get().unwrap().prefix), false);
-            e.field(format!("`{}play [name to search or url]`", CONFIG.get().unwrap().prefix), format!("Plays the specified song \n\
-            *aliases:* `{}p`", CONFIG.get().unwrap().prefix), false);
-            e.field(format!("`{}pause`", CONFIG.get().unwrap().prefix), "Pauses the current song", false);
-            e.field(format!("`{}resume`", CONFIG.get().unwrap().prefix), "Resumes the current song", false);
-            e.field(format!("`{}skip`", CONFIG.get().unwrap().prefix), format!("Skips the current song \n\
-            *aliases:* `{}s`", CONFIG.get().unwrap().prefix), false);
-            e.field(format!("`{}queue`", CONFIG.get().unwrap().prefix), format!("Shows which songs are in the queue \n\
-            *aliases:* `{0}playlist, {0}q`", CONFIG.get().unwrap().prefix), false);
-            e.field(format!("`{}playing`", CONFIG.get().unwrap().prefix), format!("Lists the current song \n\
-            *aliases:* `{0}current, {0}np`", CONFIG.get().unwrap().prefix), false);
-            e.field(format!("`{}volume [volume 1-100]`", CONFIG.get().unwrap().prefix), format!("Adjusts the volume for all users \n\
-            *aliases:* `{}vol`", CONFIG.get().unwrap().prefix), false);
-            e.field(format!("`{}stop`", CONFIG.get().unwrap().prefix), "Stops the song and disconnects from the voice channel. \n\
+            e.field(format!("`{}connect [user]`", prefix), format!("Joins the voice channel the user is in \n\
+            *aliases:* `{}join`", prefix), false);
+            e.field(format!("`{}play [name to search or url]`", prefix), format!("Plays the specified song \n\
+            *aliases:* `{}p`", prefix), false);
+            e.field(format!("`{}pause`", prefix), "Pauses the current song", false);
+            e.field(format!("`{}resume`", prefix), "Resumes the current song", false);
+            e.field(format!("`{}skip`", prefix), format!("Skips the current song \n\
+            *aliases:* `{}s`", prefix), false);
+            e.field(format!("`{}queue`", prefix), format!("Shows which songs are in the queue \n\
+            *aliases:* `{0}playlist, {0}q`", prefix), false);
+            e.field(format!("`{}playing`", prefix), format!("Lists the current song \n\
+            *aliases:* `{0}current, {0}np`", prefix), false);
+            e.field(format!("`{}volume [volume 1-100]`", prefix), format!("Adjusts the volume for all users \n\
+            *aliases:* `{}vol`", prefix), false);
+            e.field(format!("`{}stop`", prefix), "Stops the song and disconnects from the voice channel. \n\
             this will clear the entire playlist", false)
         })
     }).await?;
@@ -114,7 +115,7 @@ async fn help_music(ctx: &Context, msg: &Message) -> CommandResult {
     Ok(())
 }
 
-async fn help_utilities(ctx: &Context, msg: &Message) -> CommandResult {
+async fn help_utilities(ctx: &Context, msg: &Message, prefix: &str) -> CommandResult {
 
     let thumbnail_url = msg.guild(&ctx.cache).await.unwrap().icon_url();
 
@@ -123,9 +124,9 @@ async fn help_utilities(ctx: &Context, msg: &Message) -> CommandResult {
             e.color(CONFIG.get().unwrap().colours.commands);
             if let Some(url) = thumbnail_url {e.thumbnail(url);};
             e.title("**Help Utilities**");
-            e.field(format!("`{}say [message]`", CONFIG.get().unwrap().prefix), "Repeats what the user says", false);
-            e.field(format!("`{}poll [message]`", CONFIG.get().unwrap().prefix), "Creates a poll", false);
-            e.field(format!("`{}suggest [message]`", CONFIG.get().unwrap().prefix), "Suggests an idea to #server-suggestions", false)
+            e.field(format!("`{}say [message]`", prefix), "Repeats what the user says", false);
+            e.field(format!("`{}poll [message]`", prefix), "Creates a poll", false);
+            e.field(format!("`{}suggest [message]`", prefix), "Suggests an idea to #server-suggestions", false)
         })
     }).await?;
 
