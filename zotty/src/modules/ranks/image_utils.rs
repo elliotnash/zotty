c1f095b066a8a3b671d1ec4353d4fe3472102fc2
Elliot Nash <elliotnash@gmail.com>
2021-07-27 22:14:35 -0700

    use skia in leaderboard card

--- zotty/src/modules/ranks/image_utils.rs
+++ zotty/src/modules/ranks/image_utils.rs
@@ -1,3 +1,40 @@
+use skia_safe::{Codec, Data, Surface, Typeface};
+use std::fs;
+
+pub fn load_image_surface(bg_image: BackgroundImage) -> Surface {
+    let path = match bg_image {
+        BackgroundImage::Rank => {"rank.png"}
+        BackgroundImage::Leaderboard => {"leaderboard.png"}
+    };
+    let data = fs::read(path).unwrap();
+    let skdata = Data::new_copy(&*data);
+    let mut codec = Codec::from_data(skdata).unwrap();
+    let image = codec.get_image(None, None).unwrap();
+    let mut surface = Surface::new_raster_n32_premul((image.dimensions().width, image.dimensions().height)).expect("no surface!");
+    surface.canvas().draw_image(image, (0, 0), None);
+    surface.canvas().save();
+    surface
+}
+
+pub enum BackgroundImage{
+    Rank,
+    Leaderboard
+}
+
+pub fn load_typeface(fontweight: FontWeight) -> Typeface {
+    let path = match fontweight {
+        FontWeight::Light => {"font-light.otf"}
+        FontWeight::Regular => {"font.otf"}
+    };
+    let data = fs::read(path).unwrap();
+    let skdata = Data::new_copy(&*data);
+    Typeface::from_data(skdata, 0).unwrap()
+}
+
+pub enum FontWeight{
+    Light,
+    Regular
+}
 
 pub fn format_descriminator(discriminator: impl ToString) -> String {
     let str = discriminator.to_string();
