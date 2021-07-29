4d0d2104bfcb7e3b044183e866a38003c300a276
Elliot Nash <elliotnash@gmail.com>
2021-07-29 00:28:45 -0700

    use buildurl to form proper urls

--- zotty-web/src/index.tsx
+++ zotty-web/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.sass';
 import App from './App';
 import axios from 'axios';
+import BuildUrl from 'build-url';
 
 export const BACKEND_URL = get_backend_url();
 function get_backend_url(): string {
@@ -21,7 +22,7 @@ ReactDOM.render(
 );
 
 axios.get(
-  BACKEND_URL+"/api/ping"
+  BuildUrl(BACKEND_URL, {path: '/api/ping'})
 ).then((response) => {
-  console.log(response.data)
+  console.log(response.data);
 });
