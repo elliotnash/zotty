e4444c2cbc441c4f0c33b26cccd85c7f90d5d853
Elliot Nash <elliotnash@gmail.com>
2021-07-28 21:51:18 -0700

    send oauth info on ping route

--- zotty/src/web.rs
+++ zotty/src/web.rs
@@ -1,9 +1,22 @@
-use rocket::Config;
+use rocket::{
+    Config,
+    serde::json::Json
+};
+use serde::Serialize;
 use crate::CONFIG;
 
 #[get("/ping")]
-async fn ping() -> &'static str {
-    "pong"
+async fn ping() -> Json<OAuthInfo> {
+    Json(OAuthInfo{
+        api_url: CONFIG.get().unwrap().web.oauth.api_url.clone(),
+        client_id: CONFIG.get().unwrap().web.oauth.client_id.clone()
+    })
+}
+
+#[derive(Serialize)]
+struct OAuthInfo{
+    pub api_url: String,
+    pub client_id: String
 }
 
 pub async fn rocket() {
