301deee94001f200ee7491d192c1b9645b4b90d3
Elliot Nash <elliotnash@gmail.com>
2021-07-29 10:49:26 -0700

    redirect to oauth url

--- zotty-web/src/Login.tsx
+++ zotty-web/src/Login.tsx
@@ -1,33 +1,49 @@
 import React from "react";
+import { RouteComponentProps } from "react-router-dom";
 import axios from "axios";
 import BuildUrl from "build-url";
 import {BACKEND_URL} from ".";
 
-interface LoginProps {
+interface OAuthInfo{
+  api_url: string,
+  client_id: string
+}
+interface LoginProps extends RouteComponentProps {
 }
 interface LoginStates{
-  data: string
 }
 export default class Login extends React.Component<LoginProps, LoginStates> {
-
   constructor(props: LoginProps) {
     super(props);
-    this.state = { data : "test"};
   }
 
   render() {
     return (
-      <div>{this.state.data}</div>
+      <div></div>
     );
   }
   componentDidMount() {
+    // ping api, recieve oauth info
     axios.get(
       BuildUrl(BACKEND_URL, {path: '/api/ping'})
     ).then((response) => {
-      this.setState({
-        data: JSON.stringify(response.data),
+      let oauthInfo = response.data as OAuthInfo;
+      // construct redirect url
+      let redirectUrl = BuildUrl(window.location.origin, {
+        path: "/authorize"
       });
-      console.log(this.state.data);
+      // construct discord oauth url
+      let oauthUrl = BuildUrl(oauthInfo.api_url, {
+        path: "/oauth2/authorize",
+        queryParams: {
+          client_id: oauthInfo.client_id,
+          redirect_uri: redirectUrl,
+          response_type: "code",
+          scope: "identify guilds",
+          prompt: "consent"
+        }
+      })
+      window.location.replace(oauthUrl);
     });
   }
 }
