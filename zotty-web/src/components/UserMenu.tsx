06739a7bb4661a2a42c5be74707507df2981117a
Elliot Nash <elliotnash@gmail.com>
2021-08-08 15:39:24 -0700

    add menu item

--- zotty-web/src/components/UserMenu.tsx
+++ zotty-web/src/components/UserMenu.tsx
@@ -2,13 +2,25 @@ import React from "react";
 import { RouteComponentProps, withRouter } from "react-router-dom";
 import './UserMenu.sass';
 
-interface MenuProps extends RouteComponentProps {};
-interface MenuStates{};
-class UserMenu extends React.Component<MenuProps, MenuStates> {
+interface MenuItemProps extends RouteComponentProps {};
+interface MenuItemStates{};
+const MenuItem = withRouter(class MenuItem extends React.Component<MenuItemProps, MenuItemStates> {
+  render() {
+    return (
+      <div className="menu-item-div">
+
+      </div>
+    );
+  }
+});
+
+interface UserMenuProps extends RouteComponentProps {};
+interface UserMenuStates{};
+class UserMenu extends React.Component<UserMenuProps, UserMenuStates> {
   render() {
     return (
       <div id="menu-div">
-
+        <MenuItem/>
       </div>
     );
   }
